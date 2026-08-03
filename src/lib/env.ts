d98ed7f16106c31a3e7e7a3d3f6201c4b84194e7
Abhishek Jha <iamabhishek1310@gmail.com>
2026-08-03 19:37:34 +0530

    Configured environment variables and updated dependencies

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,2 +1,3 @@
 export const SIGNUP_URL = process.env.NEXT_PUBLIC_SIGNUP_URL as string;
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL as string;
+export const DEMO_URL = process.env.NEXT_PUBLIC_DEMO_URL as string;
